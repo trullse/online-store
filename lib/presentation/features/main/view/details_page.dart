d4a37e77720e89506b30806c468f5a02daf22700
Aliaksei <alx.leonenko@gmail.com>
2024-09-20 13:17:23 +0300

    add details page

---
+++ lib/presentation/features/main/view/details_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class DetailsPage extends StatelessWidget {
+  const DetailsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Center(
+          child: Text('Details'),
+        ),
+      ),
+      body: const Placeholder(),
+    );
+  }
+}
